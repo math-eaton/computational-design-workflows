3b98f36486a7b1b77bc767c52cc5b42a33ce6f46
math-eaton <mheaton@ciesin.columbia.edu>
2022-08-06 15:13:38 -0400

    Pre-meeting project commit
    How it is...

--- HW_website_forms/scripts.js
+++ HW_website_forms/scripts.js
@@ -1,7 +1,6 @@
 console.log('ok...')
 
 
-
 // Structure
 //----------------------------
 const form = document.querySelector("form");
@@ -79,6 +78,14 @@ function textCounter(field,field2,maxlimit)
  } else {
   countfield.value = maxlimit - field.value.length;
  }
+
+ if(countfield.value <= 10){
+  counter.style.color = 'red';
+  counter.style.fontWeight = 'bold'
+} else {
+  counter.style.color = 'black';
+  counter.style.fontWeight = 'normal'
+}
 }
 
 //error messages
@@ -102,3 +109,108 @@ form.addEventListener('submit', (e) => {
     wordsInput.innerText = messages.join(', ')
   }
 })
+
+
+//mapBox API
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWpoMjI0MSIsImEiOiJjbDZhNWdtdWcwemYwM2Nyejg4azR6MjdtIn0.jcFs6eofGMkSv7Gokq_b6A';
+const coordinates = document.getElementById('coordinates');
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [0, 0],
+    zoom: 2
+});
+
+const canvas = map.getCanvasContainer();
+
+const geojson = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [0, 0]
+            }
+        }
+    ]
+};
+
+function onMove(e) {
+    const coords = e.lngLat;
+
+    // Set a UI indicator for dragging.
+    canvas.style.cursor = 'grabbing';
+
+    // Update the Point feature in `geojson` coordinates
+    // and call setData to the source layer `point` on it.
+    geojson.features[0].geometry.coordinates = [coords.lng, coords.lat];
+    map.getSource('point').setData(geojson);
+}
+
+function onUp(e) {
+    const coords = e.lngLat;
+
+    // Print the coordinates of where the point had
+    // finished being dragged to on the map.
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = `Longitude: ${coords.lng}<br />Latitude: ${coords.lat}`;
+    canvas.style.cursor = '';
+
+    // Unbind mouse/touch events
+    map.off('mousemove', onMove);
+    map.off('touchmove', onMove);
+}
+
+map.on('load', () => {
+    // Add a single point to the map.
+    map.addSource('point', {
+        'type': 'geojson',
+        'data': geojson
+    });
+
+    map.addLayer({
+        'id': 'point',
+        'type': 'circle',
+        'source': 'point',
+        'paint': {
+            'circle-radius': 10,
+            'circle-color': '#F84C4C' // red color
+        }
+    });
+
+    // When the cursor enters a feature in
+    // the point layer, prepare for dragging.
+    map.on('mouseenter', 'point', () => {
+        map.setPaintProperty('point', 'circle-color', '#3bb2d0');
+        canvas.style.cursor = 'move';
+    });
+
+    map.on('mouseleave', 'point', () => {
+        map.setPaintProperty('point', 'circle-color', '#3887be');
+        canvas.style.cursor = '';
+    });
+
+    map.on('mousedown', 'point', (e) => {
+        // Prevent the default map drag behavior.
+        e.preventDefault();
+
+        canvas.style.cursor = 'grab';
+
+        map.on('mousemove', onMove);
+        map.once('mouseup', onUp);
+    });
+
+    map.on('touchstart', 'point', (e) => {
+        if (e.points.length !== 1) return;
+
+        // Prevent the default map drag behavior.
+        e.preventDefault();
+
+        map.on('touchmove', onMove);
+        map.once('touchend', onUp);
+    });
+});
+
